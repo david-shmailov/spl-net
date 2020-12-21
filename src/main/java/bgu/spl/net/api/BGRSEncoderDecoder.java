5d6f71548af6db276a56813e6bb79c3a5733b8a0
aviramlachmani <aviraml@post.bgu.ac.il>
2020-12-21 20:13:01 +0200

    aviram update

---
+++ src/main/java/bgu/spl/net/api/BGRSEncoderDecoder.java
@@ -0,0 +1,48 @@
+package bgu.spl.net.api;
+
+import java.util.Arrays;
+
+public class BGRSEncoderDecoder implements MessageEncoderDecoder<byte[]>{
+
+    private byte[] bytes = new byte[1 << 10]; //start with 1k
+    private int len = 0;
+
+    @Override
+    public byte[] decodeNextByte(byte nextByte) {
+        //notice that the top 128 ascii characters have the same representation as their utf-8 counterparts
+        //this allow us to do the following comparison
+        if (nextByte == '\n') {
+            return popString();
+        }
+
+        pushByte(nextByte);
+        return null; //not a line yet
+    }
+
+    @Override
+    public byte[] encode(byte[] message) {
+        byte[] send=new byte[message.length+1] ;
+        for(int i=0; i<=message.length;i++) {send[i]=bytes[i];}
+        send[message.length+1]=0;
+        return message;
+    }
+
+    private void pushByte(byte nextByte) {
+        if (len >= bytes.length) {
+            bytes = Arrays.copyOf(bytes, len * 2);
+        }
+
+        bytes[len++] = nextByte;
+    }
+
+    private byte[] popString() {
+        //notice that we explicitly requesting that the string will be decoded from UTF-8
+        //this is not actually required as it is the default encoding in java.
+        byte[] result=new byte[len] ;
+        for(int i=0; i<=len;i++) {result[i]=bytes[i];}
+        len = 0;
+        return result;
+    }
+}
+
+
--- src/main/java/bgu/spl/net/api/BGRSprotocol.java
+++ src/main/java/bgu/spl/net/api/BGRSprotocol.java
@@ -1,24 +1,91 @@
 package bgu.spl.net.api;
 
-import java.time.LocalDateTime;
+import java.nio.charset.StandardCharsets;
 
-public class BGRSprotocol implements MessagingProtocol<String> {
+public class BGRSprotocol implements MessagingProtocol<byte[]> {
     private boolean isStudent=false;
     private boolean isLogin=false;
     private boolean isAdmin=false;
     private String UserName=null;
     private boolean shouldTerminate = false;
+    private Database database=Database.getInstance();
 
     @Override
-    public String process(String msg) {
-
-        return createEcho(msg);
+    public byte[] process(byte[] msg) {
+        short result = (short)((msg[0] & 0xff) << 8); //convert the first 2 byte
+        result += (short)(msg[1] & 0xff);             //convert the first 2 byte
+        if(result==1) return AdminReg(msg);
+        if(result==2) return StudentRed(msg);
+        if(result==3) return Login(msg);
+        if(result==4) return Logout(msg);
+        if(result==5) return CourseReg(msg);
+        if(result==6) return KdamCheck(msg);
+        if(result==7) return CourseStat(msg);
+        if(result==8) return StudentStat(msg);
+        if(result==9) return IsRegistered(msg);
+        if(result==10) return UnRegister(msg);
+        if(result==11) return MyCourses(msg);
+        if(result==12) return ACK(msg);
+        if(result==13) return ERROR(msg);
+        else return null;
     }
 
-    private String createEcho(String message) {
-        String echoPart = message.substring(Math.max(message.length() - 2, 0), message.length());
-        return message + " .. " + echoPart + " .. " + echoPart + " ..";
+    private byte[] AdminReg(byte[] msg) {
+        String[] str =bytesToString(msg);
+        UserName=str[0];
+
     }
+
+    private byte[] StudentRed(byte[] msg) {
+    }
+
+    private byte[] Login(byte[] msg) {
+    }
+
+    private byte[] Logout(byte[] msg) {
+    }
+
+    private byte[] CourseReg(byte[] msg) {
+    }
+
+    private byte[] KdamCheck(byte[] msg) {
+    }
+
+    private byte[] CourseStat(byte[] msg) {
+    }
+
+    private byte[] StudentStat(byte[] msg) {
+    }
+
+    private byte[] IsRegistered(byte[] msg) {
+    }
+
+    private byte[] UnRegister(byte[] msg) {
+    }
+
+    private byte[] MyCourses(byte[] msg) {
+    }
+
+    private byte[] ACK(byte[] msg) {
+    }
+
+    private byte[] ERROR(byte[] msg) {
+    }
+
+    private String[] bytesToString(byte[] msg){
+        int start=0;
+        int index=0;
+        String[] str=new String[2];
+        for(int i=2;i<=msg.length;i++){
+            if(msg[i]==0) {
+                str[index]=new String(msg,start,i,StandardCharsets.UTF_8);
+                start=i+1;
+                index++;
+            }
+        }
+        return str;
+    }
+
 
     @Override
     public boolean shouldTerminate() {

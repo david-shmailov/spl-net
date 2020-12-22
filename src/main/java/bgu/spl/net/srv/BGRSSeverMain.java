cbb90c6d3b6f7f93e76021c3d0cb6984f21b4893
aviramlachmani <aviraml@post.bgu.ac.il>
2020-12-22 14:47:12 +0200

    aviram update

---
+++ src/main/java/bgu/spl/net/srv/BGRSSeverMain.java
@@ -0,0 +1,20 @@
+package bgu.spl.net.srv;
+
+import bgu.spl.net.api.BGRSEncoderDecoder;
+import bgu.spl.net.api.BGRSprotocol;
+import bgu.spl.net.impl.newsfeed.NewsFeed;
+
+
+public class BGRSSeverMain {
+    public static void main(String[] args) {
+        NewsFeed feed = new NewsFeed(); //one shared object
+
+        Server.threadPerClient(
+                7777, //port
+              () -> new BGRSprotocol(), //protocol factory
+              BGRSEncoderDecoder::new //message encoder decoder factory
+        ).serve();
+
+    }
+}
+

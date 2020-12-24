22dc826f7b943a1247e0503f0b11541c91912541
aviramlachmani <aviraml@post.bgu.ac.il>
2020-12-24 17:33:25 +0200

    aviram update

--- src/main/java/bgu/spl/net/api/BGRSprotocol.java
+++ src/main/java/bgu/spl/net/api/BGRSprotocol.java
@@ -1,5 +1,7 @@
 package bgu.spl.net.api;
 
+
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -15,233 +17,193 @@ public class BGRSprotocol implements MessagingProtocol<byte[]> {
 
     @Override
     public byte[] process(byte[] msg) {
-        short result = (short)((msg[0] & 0xff) << 8); //convert the first 2 byte
-        result += (short)(msg[1] & 0xff);             //convert the first 2 byte
-        if(result==1) return AdminReg(msg);
-        if(result==2) return StudentRed(msg);
-        if(result==3) return Login(msg);
-        if(result==4) return Logout(msg);
-        if(result==5) return CourseReg(msg);
-        if(result==6) return KdamCheck(msg);
-        if(result==7) return CourseStat(msg);
-        if(result==8) return StudentStat(msg);
-        if(result==9) return IsRegistered(msg);
-        if(result==10) return UnRegister(msg);
-        if(result==11) return MyCourses(msg);
+        String str[]=bytesToString(msg);    ///covert the byte[] to string []
+        int result =Integer.parseInt(str[0]);      ////convert the commend string to int
+        if(result==1) return AdminReg(str);
+        if(result==2) return StudentRed(str);
+        if(result==3) return Login(str);
+        if(result==4) return Logout(str);
+        if(result==5) return CourseReg(str);
+        if(result==6) return KdamCheck(str);
+        if(result==7) return CourseStat(str);
+        if(result==8) return StudentStat(str);
+        if(result==9) return IsRegistered(str);
+        if(result==10) return UnRegister(str);
+        if(result==11) return MyCourses(str);
     //    if(result==12) return ACK(msg); // todo check if it is needed to do
     //    if(result==13) return ERROR(msg);
-        else return sendError(msg);
+        else return sendError(str);
     }
 
-    private byte[] AdminReg(byte[] msg) {
-        String[] str =bytesToString(msg);
+    private byte[] AdminReg(String[] str) {
+        UserName=str[1];
+        if(database.AdminRegister(str[1],str[2])) return sendACK(str);
+        return sendError(str);
+    }
+
+    private byte[] StudentRed(String[] str) {
         UserName=str[0];
-        if(database.AdminRegister(str[0],str[1])) return sendACK(msg);
-        return sendError(msg);
+        if(database.studentRegister(str[1],str[2])) return sendACK(str);
+        return sendError(str);
     }
 
-    private byte[] StudentRed(byte[] msg) {
-        String[] str =bytesToString(msg);
-        UserName=str[0];
-        if(database.studentRegister(str[0],str[1])) return sendACK(msg);
-        return sendError(msg);
-    }
-
-    private byte[] Login(byte[] msg) {
-        String[] str =bytesToString(msg);
-        if(database.isStudent(str[0])){
-            if (database.LoginStudent(str[0],str[1])) {
-                isLoginAdmin=true;
-                return sendACK(msg);
+    private byte[] Login(String[] str) {
+        if(database.isStudent(str[1])){
+            if (database.LoginStudent(str[1],str[2])) {
+                isLoginStudent=true;
+                return sendACK(str);
             }
         }
         else{
-            if(database.LoginAdmin(str[0],str[1])) {
-                isLoginStudent=true;
-                return sendACK(msg);
+            if(database.LoginAdmin(str[1],str[2])) {
+                isLoginAdmin=true;
+                return sendACK(str);
             }
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] Logout(byte[] msg) {
+    private byte[] Logout(String[] str) {
         if(isLoginAdmin|| isLoginStudent){
             database.Logout(UserName);
             shouldTerminate=true;
-            return sendACK(msg);
+            return sendACK(str);
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] CourseReg(byte[] msg) {
-        short courseNum = (short)((msg[2] & 0xff) << 8); //convert the 2 byte
-        courseNum += (short)(msg[3] & 0xff);             //convert the 2 byte
-        if(isLoginStudent && database.CourseRegister(courseNum,UserName))return sendACK(msg);
-        return sendError(msg);
+    private byte[] CourseReg(String[] str) {
+        int courseNum=Integer.parseInt(str[1]);
+        if(isLoginStudent && database.CourseRegister(courseNum,UserName))return sendACK(str);
+        return sendError(str);
     }
 
-    private byte[] KdamCheck(byte[] msg) {
+    private byte[] KdamCheck(String[] str) {
         if(isLoginStudent) {
-            short courseNum = (short) ((msg[2] & 0xff) << 8); //convert the 2 byte
-            courseNum += (short) (msg[3] & 0xff);             //convert the 2 byte
+            int courseNum=Integer.parseInt(str[1]);
             if(database.KdamNeeded(courseNum)!=null)
-            return sendACKOptionalList(msg, database.KdamNeeded(courseNum)); //todo list of kdam need to be order
+            return sendACKOptionalList(str, database.KdamNeeded(courseNum)); //todo list of kdam need to be order
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] CourseStat(byte[] msg) {
+    private byte[] CourseStat(String[] str) {
         if(isLoginAdmin){
-            short courseNum = (short) ((msg[2] & 0xff) << 8); //convert the 2 byte
-            courseNum += (short) (msg[3] & 0xff);             //convert the 2 byte
-            return sendACKCourseStat(msg,courseNum,database.courseName(courseNum),database.SeatsMax(courseNum),
+            int courseNum=Integer.parseInt(str[1]);
+            return sendACKCourseStat(str,courseNum,database.courseName(courseNum),database.SeatsMax(courseNum),
                     database.SeatsCurrent(courseNum),database.StudentsRegisterToCourse(courseNum));//todo list of student need to be order alphabetic
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] StudentStat(byte[] msg) {
+    private byte[] StudentStat(String[] str) {
         if(isLoginAdmin){
-            String name=bytesToString(msg)[0];
-            return sendACKStringAndList(msg,name,database.StudentStat(name));//todo list of Integer need to be order
+            String name=str[1];
+            return sendACKStringAndList(str,name,database.StudentStat(name));//todo list of Integer need to be order
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] IsRegistered(byte[] msg) {
+    private byte[] IsRegistered(String[] str) {
         if(isLoginStudent){
-            short courseNum = (short) ((msg[2] & 0xff) << 8); //convert the 2 byte
-            courseNum += (short) (msg[3] & 0xff);             //convert the 2 byte
-            return sendACKOptionalString(msg,database.isRegistered(courseNum,UserName));
+            int courseNum=Integer.parseInt(str[1]);
+            return sendACKOptionalString(str,database.isRegistered(courseNum,UserName));
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] UnRegister(byte[] msg) {
+    private byte[] UnRegister(String[] str) {
         if (isLoginStudent){
-            short courseNum = (short) ((msg[2] & 0xff) << 8); //convert the 2 byte
-            courseNum += (short) (msg[3] & 0xff);             //convert the 2 byte
-            if (database.unregistered(courseNum,UserName)) return sendACK(msg);
+            int courseNum=Integer.parseInt(str[1]);
+            if (database.unregistered(courseNum,UserName)) return sendACK(str);
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] MyCourses(byte[] msg) {
+    private byte[] MyCourses(String[] str) {
         if(isLoginStudent){
-            return sendACKOptionalList(msg,database.myCourses(UserName));
+            return sendACKOptionalList(str,database.myCourses(UserName));
         }
-        return sendError(msg);
+        return sendError(str);
     }
 
-    private byte[] sendACK(byte[] msg){
+    private byte[] sendACK(String[] str){
         byte[] send=new byte[4];
         short num=12;
         send[0] = (byte)((num >> 8) & 0xFF);
         send[1] = (byte)(num & 0xFF);
-        send[2] = msg[0];
-        send[3] = msg[1];
+        byte[] b=str[0].getBytes();
+        send= Arrays.copyOf(send, send.length+b.length);
+        for(int i=send.length-b.length;i<send.length;i++)
+            send[i+b.length]=b[i];
         return send;
     }
-    private byte[] sendACKCourseStat(byte[] msg,short numCourse,String courseName, int seatsMax, int seatsCurrent, LinkedList<String> list) {
-        byte[] send=sendACK_Short_String(msg,numCourse,courseName);
-        int index=send.length;
-        send= Arrays.copyOf(send, send.length+6);
-        send[index] = (byte)(((seatsCurrent >> 8) & 0xFF));
-        send[index+1] = (byte)(seatsCurrent & 0xFF);
-        send[index+2] = 0;
-        send[index+3] = (byte)(((seatsMax >> 8) & 0xFF));
-        send[index+4] = (byte)(seatsMax & 0xFF);
-        send[index+5] = 0;
-        return getListOfStringToBytes(list,send,index+6);
-    }
-
-    private byte[] getListOfStringToBytes(LinkedList<String> list, byte[] send, int index) {
-        for(String name:list){
-            byte[] Name=name.getBytes();
-            send=Arrays.copyOf(send,send.length+Name.length);//todo mybe to copy the array different
-            for(int i=0;i<Name.length;i++){
-                send[i+index]=Name[i];
-            }
-            index=index+Name.length;
+    private byte[] sendACKCourseStat(String[] str,int numCourse,String courseName, int seatsMax, int seatsCurrent, LinkedList<String> list) {
+        byte[] send=sendACK_Short_String(str,numCourse,courseName);
+        byte[] curr=String.valueOf(seatsCurrent).getBytes();
+        byte[] max=String.valueOf(seatsMax).getBytes();
+        send=unionByte(send,curr);
+        send=unionByte(send,max);
+        for(String name: list){
+            byte[] nam=name.getBytes();
+            send=unionByte(send,nam);
         }
         return send;
     }
 
-    private byte[] sendACKStringAndList(byte[] msg, String name, LinkedList<Integer> list) {
-        byte[] send=sendACKOptionalString(msg,name);
-        send= Arrays.copyOf(send, send.length+3*list.size());
-        int index=0;
-        for (int j=0;j<send.length;j++){
-            if(send[j]==0) {index =j; break;}
-        }
-        return getListToBytes(list, send, index+1);
-    }
 
-    private byte[] getListToBytes(LinkedList<Integer> list, byte[] send, int index) {
-        for (Integer course:list){
-            short sCourse= (short) (course& 0x00FF);
-            send[index] = (byte)(((sCourse >> 8) & 0xFF));
-            send[index+1] = (byte)(sCourse & 0xFF);
-            send[index+2]=0;
-            index=index+3;
+    private byte[] sendACKStringAndList(String[] str, String name, LinkedList<Integer> list) {
+        byte[] send=sendACKOptionalString(str,name);
+        for (Integer course: list){
+            byte[] cours=String.valueOf(course).getBytes();
+            send=unionByte(send,cours);
         }
         return send;
     }
 
-    private byte[] sendACKOptionalList(byte[] msg, LinkedList<Integer> list) {
-        byte[] send =new byte[4+3*list.size()];
-        short num=12;
-        send[0] = (byte)((num >> 8) & 0xFF);
-        send[1] = (byte)(num & 0xFF);
-        send[2]=msg[0];
-        send[3]=msg[1];
-        int index=4;        ///optional part
-        return getListToBytes(list, send, index);
-    }
-    private byte[] sendACKOptionalString(byte[] msg, String str) {
-        byte[] st=str.getBytes();
-        byte[] send =new byte[4+st.length];
-        short num=12;
-        send[0] = (byte)((num >> 8) & 0xFF);
-        send[1] = (byte)(num & 0xFF);
-        send[2]=msg[0];
-        send[3]=msg[1];
-        for (int i=4;i<4+st.length;i++){    ///optional part
-           send[i]=st[i-4];
+    private byte[] sendACKOptionalList(String[] str, LinkedList<Integer> list) {
+        byte[] send =sendACK(str);
+        for (Integer course: list){
+            byte[] cours=String.valueOf(course).getBytes();
+            send=unionByte(send,cours);
         }
         return send;
     }
-    private byte[] sendACK_Short_String(byte[] msg,short numCourse, String str) {
-        byte[] st=str.getBytes();
-        byte[] send =new byte[6+st.length];
-        short num=12;
-        send[0] = (byte)((num >> 8) & 0xFF);
-        send[1] = (byte)(num & 0xFF);
-        send[2]=msg[0];
-        send[3]=msg[1];
-        send[4]=(byte)((numCourse >> 8) & 0xFF);
-        send[5]=(byte)(numCourse & 0xFF);
-        for (int i=4;i<4+st.length;i++){    ///optional part
-            send[i]=st[i-4];
-        }
-        return send;
+    private byte[] sendACKOptionalString(String[] str, String name) {
+        byte[] st=name.getBytes();
+        byte[] send =sendACK(str);
+        return unionByte(send,st);
     }
-    private byte[] sendError(byte[] msg){
+    private byte[] sendACK_Short_String(String[] str,int numCourse, String name) {
+        byte[] name1=name.getBytes();
+        byte[] num=String.valueOf(numCourse).getBytes();
+        byte[] send =sendACK(str);
+        send=unionByte(send,num);
+        return unionByte(send,name1);
+    }
+    private byte[] unionByte(byte[] one,byte[] two){
+        int index=one.length;
+        one= Arrays.copyOf(one, one.length+two.length);
+        for(int i=index;i<one.length;i++){
+            one[i]=two[i-index];
+        }
+        return one;
+    }
+    private byte[] sendError(String[] str){
         byte[] send=new byte[4];
         short num=13;
         send[0] = (byte)((num >> 8) & 0xFF);
         send[1] = (byte)(num & 0xFF);
-        send[2]=msg[0];
-        send[3]=msg[1];
-        return send;
+        byte[] b=str[0].getBytes();
+        return unionByte(send,b);
     }
 
     private String[] bytesToString(byte[] msg){
-        int start=2;
+        int start=0;
         int index=0;
         String[] str=new String[2];
-        for(int i=2;i<msg.length;i++){
-            if(msg[i]==32) {
+        for(int i=0;i<msg.length;i++){
+            if(msg[i]==32) { // if it is find ' ' namespace
                 str[index]=new String(msg,start,i-start,StandardCharsets.UTF_8);
                 start=i+1;
                 index++;
